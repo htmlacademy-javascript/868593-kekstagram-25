ac5efcffcd85e301f39dc3df25acface9a2caa74
Виталий Вислоушкин <voron13p-p@mail.ru>
2022-02-24 11:01:15 +0300

    Убрал пробелы и поменял ковычки

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
   if (min < 0) {
-  return console.log("отрицательное значение min");
-  };
+    return console.log('отрицательное значение min');
+  }
   max = Math.floor(max);
   if (max < 0) {
-  return console.log("отрицательное значение max");
-  };
+    return console.log('отрицательное значение max');
+  }
   if (min > max) {
-  return console.log("Минимальное значение больше максимального");
-  };
+    return console.log('Минимальное значение больше максимального');
+  }
   return console.log( Math.floor(Math.random() * (max - min + 1)) + min);
-};
+}
+getRandomNumber();
 
 function getCommentFieldSize(commentField ,maxLength) {
   const string = document.querySelector(commentField);
   const counter = string.value.length;
   if (counter < maxLength) {
-return console.log("true");
+    return console.log('true');
   }
- return console.log("false");
-};
+  return console.log('false');
+}
+getCommentFieldSize();
