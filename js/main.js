c21f7627b3b5f37a80b3302a543cca3a2672e4c9
Виталий Вислоушкин <voron13p-p@mail.ru>
2022-03-04 15:22:15 +0300

    исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const DESCRIPTION_ARRAY = [
+const DESCRIPTIONS = [
   'Пляж',
   'Указатель',
   'Море',
@@ -25,7 +25,7 @@ const DESCRIPTION_ARRAY = [
   'Концерт',
   'Сафари'
 ];
-const MESSAGE = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо.',
   'Но не всё.',
@@ -46,10 +46,14 @@ const NAMES = [
   'Люпита',
   'Вашингтон',
 ];
-const ID_ARRAY = createArray(25);
-const IMG_ARRAY = createArray(25);
-const AVATAR_ARRAY = createArray(6);
 const PHOTOS_COUNT = 25;
+const MINIMAL_LIKES = 15;
+const MAXIMAL_LIKES = 200;
+const AVATAR_COUNT = 6;
+const ids = createArray(PHOTOS_COUNT);
+const imgs = createArray(PHOTOS_COUNT);
+const avatars= createArray(AVATAR_COUNT);
+
 
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
@@ -71,7 +75,6 @@ function getRandomNumber(min, max) {
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getRandomNumber(12,15);
 
 function getCommentFieldSize(commentField ,maxLength) {
   return commentField.length <= maxLength;
@@ -86,10 +89,10 @@ function createArray (lengthArray) {
   }
   return array;
 }
-createArray(14);
 
 function getRandomElement (elements)  {
-  return elements[getRandomNumber(0, elements.length - 1)];
+  const elementsCopy = elements.slice();
+  return elementsCopy[getRandomNumber(0, elementsCopy.length - 1)];
 }
 
 function shuffle(arr){
@@ -103,22 +106,12 @@ function shuffle(arr){
   }
   return arr;
 }
-shuffle(ID_ARRAY);
-
-function getUrl (numberImg) {
-  const imgNumber = numberImg;
-  const string = 'photos/';
-  const format = '.jpg';
-  const url = string + imgNumber + format;
-  return url;
-}
-getUrl(IMG_ARRAY);
 
 function getDescription(numberDescription) {
-  const description =  DESCRIPTION_ARRAY[numberDescription];
+  const description =  DESCRIPTIONS[numberDescription];
   return description;
 }
-getDescription(DESCRIPTION_ARRAY);
+getDescription(DESCRIPTIONS);
 
 function getMessage(listMessage) {
   const firstSentence = getRandomElement(listMessage);
@@ -128,42 +121,34 @@ function getMessage(listMessage) {
 }
 
 function getName(listName) {
-  const name = getRandomElement(listName);
-  return name;
-}
-
-function getRandomAvatar(listAvatar) {
-  const randomAvatar = getRandomElement(listAvatar);
-  const string = 'img/avatar-';
-  const format = '.svg';
-  const url = string + randomAvatar + format;
-  return url;
-}
-
-function getPhotos () {
-  const randomArray = shuffle(ID_ARRAY);
-  for (let i = 0;i < randomArray.length;i++) {
-    const id = randomArray[i];
-    const photo = {
-      id: id,
-      url: getUrl(id),
-      description: getDescription(id),
-      likes: getRandomNumber(15,200),
-      comments: createComment(),
-    };
-    return photo;
-  }
+  return getRandomElement(listName);
 }
 
 function createComment() {
   return {
     id: getRandomNumber(0,1000),
-    avatar: getRandomAvatar(AVATAR_ARRAY),
-    message: getMessage(MESSAGE),
+    avatar: `img/avatar-${ getRandomNumber(0,AVATAR_COUNT) }.svg`,
+    message: getMessage(MESSAGES),
     name: getName(NAMES),
   };
 }
-createComment();
+const comments = Array.from({length: getRandomNumber(0,4)}, createComment);
+
+function getPhotos () {
+  const randomArray = shuffle(ids);
+  for (let i = 0;i < randomArray.length;i++) {
+    const id = randomArray[i];
+    const photo = {
+      id: id,
+      url: `photos/${ id }.jpg`,
+      description: getDescription(id),
+      likes: getRandomNumber(MINIMAL_LIKES,MAXIMAL_LIKES),
+      comments: comments,
+    };
+    return photo;
+  }
+}
+
 // eslint-disable-next-line no-unused-vars
 const photos = Array.from({length: PHOTOS_COUNT}, getPhotos);
 
