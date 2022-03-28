5f1569593f7e93626249f64fa848e5a6b6941133
Виталий Вислоушкин <voron13p-p@mail.ru>
2022-03-28 21:45:32 +0300

    Исправил ошибки

--- js/uploadImg.js
+++ js/uploadImg.js
@@ -6,8 +6,11 @@ const popupOverlayCloseBtn = document.querySelector('.img-upload__cancel');
 const hashtagsField = document.querySelector('.text__hashtags');
 const descriptionField = document.querySelector('.text__description');
 
+const onHashTagsFieldFocus = () => document.activeElement === hashtagsField;
+const onDesriptionsFieldFocus = () => document.activeElement === descriptionField;
+
 const onPopupOverlayEscKeydown = (evt) =>  {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt) && !onHashTagsFieldFocus() && !onDesriptionsFieldFocus()) {
     evt.preventDefault();
     closePopupOverlay();
   }
@@ -23,8 +26,6 @@ function closePopupOverlay () {
   document.querySelector('body').classList.remove('modal-open');
   imgUploadInput.value = '';
   document.removeEventListener('keydown', onPopupOverlayEscKeydown);
-  document.removeEventListener('focus', hashtagsField);
-  document.removeEventListener('focus', descriptionField);
 }
 
 imgUploadInput.addEventListener ('change', () => {
