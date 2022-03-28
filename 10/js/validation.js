500895ca059c368145ab678c22a4bbfb263694b0
Keks <keks@htmlacademy.ru>
2022-03-28 21:46:32 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/validation.js
+++ 10/js/validation.js
@@ -1,11 +1,8 @@
-import { getCommentFieldSize } from './util.js';
 const MAX_LENGTH_HASHTAG = 20;
-const MAX_LENGTH_COMMENTS = 140;
 const MAX_QUANTITY_HASHTAGS = 5;
 const REGEX_HASHTAG = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 const formUploadPhoto = document.querySelector('.img-upload__form');
 const hashTagsField = document.querySelector('.text__hashtags');
-const CommentField = document.querySelector('.text__description');
 
 function getHashTags() {
   const hashTagsString = hashTagsField.value.toUpperCase();
@@ -33,33 +30,22 @@ function validHashTagsRegex(arr) {
 
 function validHashTagsDubllicate(array) {
   const uniqueHashTags = array.filter((val,ind,arr) => arr.indexOf(val) === ind);
-  if(uniqueHashTags.length !== array.length) {
+  return uniqueHashTags.length === array.length;
+}
+
+function testValid() {
+  const hashTags = getHashTags();
+  if (validCountHashTags(hashTags,MAX_QUANTITY_HASHTAGS) && validHashTagsRegex(hashTags) && !validHashTagsDubllicate(hashTags) && validHashTagsElementLength(hashTags)) {
+    hashTagsField.classList.add('error__field');
     return false;
   }
   else {
+    hashTagsField.classList.remove('error__field');
     return true;
   }
 }
-
 formUploadPhoto.addEventListener('submit', (evt) => {
-  const hashTags = getHashTags();
-
-  if(!testValid()) {
-    evt.preventDefault();
-  }
-  function testValid() {
-    if(validCountHashTags(hashTags,MAX_QUANTITY_HASHTAGS) && validHashTagsRegex(hashTags) && validHashTagsDubllicate(hashTags) && validHashTagsElementLength(hashTags)) {
-      hashTagsField.classList.remove('error__field');
-      return true;
-    }
-    else {
-      hashTagsField.classList.add('error__field');
-    }
-  }
-  testValid();
-});
-formUploadPhoto.addEventListener('submit', (evt) => {
-  if(getCommentFieldSize(CommentField,MAX_LENGTH_COMMENTS)) {
+  if(!testValid()){
     evt.preventDefault();
   }
 });
